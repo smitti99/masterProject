4f392d43ad386c5411ae1a1611c82f10d66ae1c6
smitti99 <marius-schmidt11@gmx.de>
2024-01-12 00:14:44 +0100

    Added Evaluation of Log-Files

---
+++ Evaluate_Logs.py
@@ -0,0 +1,11 @@
+import BaseFunctionality.Display
+from BaseFunctionality import Display
+
+def main():
+    BaseFunctionality.Display.plot_growth()
+    BaseFunctionality.Display.plot_yield()
+
+
+
+if __name__ == "__main__":
+    main()
