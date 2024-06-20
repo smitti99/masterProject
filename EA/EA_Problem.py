3fd565dd9c5ce47fa3f77dbce574a66cd2fb1d0d
smitti99 <marius-schmidt11@gmx.de>
2024-06-20 15:07:21 +0200

    Adjusted for MultiYear

--- EA/EA_Problem.py
+++ EA/EA_Problem.py
@@ -70,6 +70,103 @@ class Field_Optimization(Problem):
         gc.collect()
 
 
+class Field_Optimization_Multi_Year(Problem):
+
+    def __init__(self, Nutrition):
+        # 3 Year-setup 0:75 = Plants | 75: = Fertilizer
+        super().__init__(84, 3)
+        self.types[0:75] = Integer(0, 3)
+        self.types[75:] = Real(0, 2)
+        self.directions[1:] = Problem.MINIMIZE
+        self.directions[0] = Problem.MAXIMIZE
+        self.nutrition = Nutrition
+        GlobalConfig.log_nutrition = False
+        GlobalConfig.log_yield = False
+        GlobalConfig.log_growth = False
+
+    def evaluate(self, solution):
+        # exract plants from variables
+        plant_list = Plant.create_plant_list()
+
+
+        # setup grid for simulation
+        grid_nutrition = HelperFunctions.convert_nutrition_array(self.nutrition)
+        g = Grid.Grid()
+        g.l_nutrition = False
+        g.l_yield = False
+        g.to_fertilize = True
+
+        fertilizer_list = solution.variables[75:78]
+        fertilizer = NutritionTable.NutritionTable(fertilizer_list[0], fertilizer_list[1], fertilizer_list[2])
+        g.fertilizer = fertilizer
+
+        plants_index = solution.variables[:25]
+        plants = [[plant_list[0][(0, 0)] for y in range(5)] for x in range(5)]
+        for x in range(5):
+            for y in range(5):
+                plants[x][y] = plant_list[0][(plants_index[x * 5 + y], 0)]
+        g.set_plants(plants)
+        g.set_nutrition(grid_nutrition)
+        # simulate Year 1
+
+        for i in range(GlobalConfig.max_steps):
+            g.step(GlobalConfig.time_mult)
+
+        #evaluate year 1
+        plant_yield = 0
+        for x in range(5):
+            for y in range(5):
+                plant_yield += g.cells[x][y].plant.harvest
+        cost = HelperFunctions.nutrition_to_cost(g.fertilizer) * 25 * GlobalConfig.max_steps
+        #setup Year 2
+        fertilizer_list = solution.variables[78:81]
+        fertilizer = NutritionTable.NutritionTable(fertilizer_list[0], fertilizer_list[1], fertilizer_list[2])
+        g.fertilizer = fertilizer
+        plants_index = solution.variables[25:50]
+        plants = [[plant_list[0][(0, 0)] for y in range(5)] for x in range(5)]
+        for x in range(5):
+            for y in range(5):
+                plants[x][y] = plant_list[0][(plants_index[x * 5 + y], 0)]
+        g.set_plants(plants)
+
+        # simulate Year 2
+        for i in range(GlobalConfig.max_steps):
+            g.step(GlobalConfig.time_mult)
+        # evaluate year 2
+        for x in range(5):
+            for y in range(5):
+                plant_yield += g.cells[x][y].plant.harvest
+        cost += HelperFunctions.nutrition_to_cost(g.fertilizer) * 25 * GlobalConfig.max_steps
+        # setup Year 3
+        fertilizer_list = solution.variables[81:]
+        fertilizer = NutritionTable.NutritionTable(fertilizer_list[0], fertilizer_list[1], fertilizer_list[2])
+        g.fertilizer = fertilizer
+        plants_index = solution.variables[50:75]
+        plants = [[plant_list[0][(0, 0)] for y in range(5)] for x in range(5)]
+        for x in range(5):
+            for y in range(5):
+                plants[x][y] = plant_list[0][(plants_index[x * 5 + y], 0)]
+        g.set_plants(plants)
+
+        # simulate Year 3
+        for i in range(GlobalConfig.max_steps):
+            g.step(GlobalConfig.time_mult)
+
+        # evaluate end_state
+        nutrition_error = 0
+        for x in range(5):
+            for y in range(5):
+                plant_yield += g.cells[x][y].plant.harvest
+                nutrition_error += HelperFunctions.evaluate_nutrition_value(g.cells[x][y].nutrition)
+
+        plant_yield /= 25
+        nutrition_error /= 25
+        cost += HelperFunctions.nutrition_to_cost(g.fertilizer) * 25 * GlobalConfig.max_steps
+
+        solution.objectives[:] = [plant_yield, nutrition_error, cost]
+        del g
+        gc.collect()
+
 class personal_NSGAII(NSGAII):
     def __init__(self, problem,
                  population_size=100,
